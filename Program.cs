84cd8bcde313c92d96047c4579e39b2e1b39927e
sonyphoebeb <sonyphoebebaree@gmail.com>
2026-03-01 16:37:32 +0530

    feat: add single dimensional array example with foreach loop

--- Program.cs
+++ Program.cs
@@ -6,10 +6,19 @@
         {
             int[] numbers = { 10, 20, 30, 40, 50 };
 
-            for(int i=0; i< numbers.Length-1; i++)
+            Console.WriteLine("Using for loop:");
+            for (int i=0; i< numbers.Length; i++)
             {
                 Console.WriteLine(numbers[i]);
             }
+
+            Console.WriteLine("Using foreach loop:");
+            foreach(int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("Array Length: " +numbers.Length);
         }
     }
 }
