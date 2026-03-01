d920837b8e15c07db4479f2b752ae70488daf01c
sonyphoebeb <sonyphoebebaree@gmail.com>
2026-03-01 15:50:49 +0530

    feat: add single dimensional array example with for loop

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DotNetArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 20, 30, 40, 50 };
+
+            for(int i=0; i< numbers.Length-1; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
