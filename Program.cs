d22c0698d1de1f009fc6c8e2e30b3dd5e02a1b43
sonyphoebeb <sonyphoebebaree@gmail.com>
2026-03-01 16:47:41 +0530

    feat: add string array examples with static input

--- Program.cs
+++ Program.cs
@@ -12,13 +12,27 @@
                 Console.WriteLine(numbers[i]);
             }
 
+            Console.WriteLine();
             Console.WriteLine("Using foreach loop:");
             foreach(int number in numbers)
             {
                 Console.WriteLine(number);
             }
 
+            Console.WriteLine();
             Console.WriteLine("Array Length: " +numbers.Length);
+
+            Console.WriteLine();
+            Console.WriteLine("Using foreach loop with string array:");
+
+            string[] technologies = { "C#", "Java", "Python", "JavaScript" };
+            foreach (string techs in technologies)
+            {
+              Console.WriteLine(techs);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("String Array Length: " + technologies.Length);
         }
     }
 }
