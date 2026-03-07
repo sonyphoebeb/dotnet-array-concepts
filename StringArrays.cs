5613c37bb7dbb8adfe97b6c066d9ed456f1a5311
sonyphoebeb <sonyphoebebaree@gmail.com>
2026-03-07 12:54:56 +0530

    Add StringArrays class with method to display names using string array and foreach loop

---
+++ StringArrays.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DotNetArrays
+{
+    public class StringArrays
+    {
+            public void DisplayNames()
+            { 
+                string[] names = { "sony", "nani", "rambabu", "jyothi" };
+
+                foreach (string name in names)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+    }
+}
+    
